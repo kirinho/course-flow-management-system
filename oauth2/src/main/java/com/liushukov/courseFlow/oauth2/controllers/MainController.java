a3aeb3b1e90a76aa2a42e679c0dc135c2efe43b4
kirinho <lushukov04@gmail.com>
2024-09-18 11:51:17 +0300

    Update oauth2 microservice

--- oauth2/src/main/java/com/liushukov/courseFlow/oauth2/controllers/MainController.java
+++ oauth2/src/main/java/com/liushukov/courseFlow/oauth2/controllers/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +20,7 @@ public class MainController {
         this.authenticationService = authenticationService;
     }
 
-    @GetMapping("/login-oauth2")
+    @PostMapping("/login-oauth2")
     public ResponseEntity<String> authentication(@AuthenticationPrincipal OAuth2User oAuth2User) {
         String email = oAuth2User.getAttribute("email");
         String name = oAuth2User.getAttribute("given_name");
